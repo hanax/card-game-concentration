690d49d7f25e0a16ccf9e35122915f9afa8d73cf
hanax <alfalfa.xue@gmail.com>
2016-10-07 00:07:47 -0400

    final clean up

--- src/scripts/classes/Game.js
+++ src/scripts/classes/Game.js
@@ -17,6 +17,8 @@ export default class Game {
 
   set hintsLeft(hintsLeft) {
     this._hintsLeft = hintsLeft;
+
+    // Update background indicator for number of hints left
     const $columnHint =
       $('button#column-hint')
       .css(
@@ -38,15 +40,13 @@ export default class Game {
       this.end();
     }
 
+    // Update displayed left time
     const initialFlashTime = INITIAL_FLASH_TIME[this.boardSize] - this.totTime + this.timeLeft;
-    if (initialFlashTime > 0
-      && !this.isPlaying()
-    ) {
-      $('#row-timer').text(`Feel the cats! ${initialFlashTime}`);
-    } else {
-      $('#row-timer').text(this.timeLeft);
-    }
+    $('#row-timer').text(initialFlashTime > 0 && !this.isPlaying()
+      ? `Feel the cats! ${initialFlashTime}`
+      : this.timeLeft);
 
+    // Update background indicator for time left
     const $rowTimer = $('.row-timer')
       .removeClass('animated-highlight')
       .css('width', (this.timeLeft / this.totTime) * 100 + '%');
@@ -60,6 +60,7 @@ export default class Game {
   }
 
   set score(score) {
+    // Animate displayed score if it goes up
     const prevScore = this.score;
     const $rowScore = $('#row-score')
       .removeClass('animated-up')
@@ -68,7 +69,7 @@ export default class Game {
       setTimeout(() => { $rowScore.addClass("animated-up") }, 10);
     }
 
-    // Update top score
+    // Update top score board
     const topScore = Math.max(
       this.score,
       window.localStorage.getItem('concentrationTopScore') || 0
@@ -96,6 +97,7 @@ export default class Game {
   showHint() {
     this.hintsLeft --;
 
+    // Find a pair to reveal
     const $enabled = getInputs($('.board'), 'enabled');
     const enabledValues = $.makeArray($enabled.map((i, obj) => obj.value));
     const possiblePair = [$enabled[0], $enabled[enabledValues.lastIndexOf(enabledValues[0])]];
@@ -104,10 +106,10 @@ export default class Game {
   }
 
   end() {
-    setInputs($(".board input"), {disabled: true});
+    setInputs(getInputs($('.board')), {disabled: true});
     this.timer = null;
 
-    // Add the time left as bonus score
+    // Add left time as bonus score
     const calScoreTimer = setInterval(() => {
       if (this.timeLeft === 0) {
         clearInterval(calScoreTimer);
@@ -140,8 +142,8 @@ export default class Game {
       for (let j = 0; j < this.boardSize; j ++) {
         const idx = i * this.boardSize + j;
         $row
-          .append(`<input type='checkbox' id='checkbox-${idx}' value='${iconIndexes[idx]}'>`)
           .append(`
+            <input type='checkbox' id='checkbox-${idx}' value='${iconIndexes[idx]}'>
             <label class='board__column' for='checkbox-${idx}'>
               <div class='board__column-wrapper'>
                 <span class='flaticon-animal-${iconIndexes[idx]}' />
@@ -155,6 +157,7 @@ export default class Game {
       const $checked = getInputs($board, 'checked');
       if ($checked.length === 2) {
         if ($checked[0].value === $checked[1].value) {
+          // Succeeded to pair
           setTimeout(() => {
             setInputs($checked, {disabled: true});
             if (getInputs($board, 'disabled').length === this.boardSize * this.boardSize) {
@@ -164,13 +167,14 @@ export default class Game {
           this.timeLeft += 5;
           this.score += 10;
         } else {
+          // Failed to pair
           this.score -= 1;
         }
         setTimeout(() => { setInputs($checked, {checked: false}); }, 500);
       }
     });
 
-    // Show the board at the beginning
+    // Show the board briefly at the beginning
     setInputs(getInputs($board), {disabled: true});
     window.setTimeout(() => {
       setInputs(getInputs($board), {disabled: false});
